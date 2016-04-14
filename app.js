b0133e57fe4db24ded8cf80deca3d4fb17639a67
Tim Carlson <tim.carlson@velocloud.net>
2016-04-13 21:32:47 -0400

    updated templates

--- app.js
+++ app.js
@@ -3,41 +3,71 @@ const http         = require('http'),
       path         = require('path'),
       contentTypes = require('./utils/content-types'),
       sysInfo      = require('./utils/sys-info'),
-      env          = process.env;
+      env          = process.env,
+      express      = require('express'),
+      passport     = require('passport'),
+      Strategy     = require('passport-local').Strategy,
+      auth         = require('./auth');
 
-let server = http.createServer(function (req, res) {
-  let url = req.url;
-  if (url == '/') {
-    url += 'index.html';
-  }
-
-  // IMPORTANT: Your application HAS to respond to GET /health with status 200
-  //            for OpenShift health monitoring
-
-  if (url == '/health') {
-    res.writeHead(200);
-    res.end();
-  } else if (url.indexOf('/info/') == 0) {
-    res.setHeader('Content-Type', 'application/json');
-    res.setHeader('Cache-Control', 'no-cache, no-store');
-    res.end(JSON.stringify(sysInfo[url.slice(6)]()));
-  } else {
-    fs.readFile('./static' + url, function (err, data) {
-      if (err) {
-        res.writeHead(404);
-        res.end();
-      } else {
-        let ext = path.extname(url).slice(1);
-        res.setHeader('Content-Type', contentTypes[ext]);
-        if (ext === 'html') {
-          res.setHeader('Cache-Control', 'no-cache, no-store');
-        }
-        res.end(data);
-      }
+passport.use(new Strategy(
+  function(username, password, cb) {
+    auth.users.findByUsername(username, function(err, user) {
+      if (err) { return cb(err); }
+      if (!user) { return cb(null, false); }
+      if (user.password != password) { return cb(null, false); }
+      return cb(null, user);
     });
-  }
+  })
+);
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user.id);
 });
 
-server.listen(env.NODE_PORT || 3000, env.NODE_IP || 'localhost', function () {
+passport.deserializeUser(function(id, cb) {
+  auth.users.findById(id, function (err, user) {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
+
+const app = express();
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(require('morgan')('combined'));
+app.use(require('cookie-parser')());
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('express-session')({ secret: 'timnmaddy', resave: false, saveUninitialized: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static('public'));
+
+app.get('/health', function(req, res) {
+  res.status(200).send(200);
+});
+
+app.get('/', function(req, res) {
+  res.render('home', {user: req.user});
+});
+
+app.get('/login', function(req, res) {
+  res.render('login');
+});
+
+app.post('/login', passport.authenticate('local', {failureRedirect: '/login'}), function(req, res) {
+  res.redirect('/');
+});
+
+app.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/');
+});
+
+app.get('/profile', require('connect-ensure-login').ensureLoggedIn(), function(req, res) {
+  res.render('profile', {user: req.user});
+});
+
+
+app.listen(env.NODE_PORT || 3000, env.NODE_IP || 'localhost', function () {
   console.log(`Application worker ${process.pid} started...`);
 });
---
+++ auth/users.js
@@ -0,0 +1,27 @@
+var records = [
+    { id: 1, username: 'tim', password: 'ilovemaddy', displayName: 'Tim', emails: [ { value: 'tim.carlson@gmail.com' } ] }
+  , { id: 2, username: 'maddy', password: 'ilovetim', displayName: 'Maddy', emails: [ { value: 'mkundel91@gmail.com' } ] }
+];
+
+exports.findById = function(id, cb) {
+  process.nextTick(function() {
+    var idx = id - 1;
+    if (records[idx]) {
+      cb(null, records[idx]);
+    } else {
+      cb(new Error('User ' + id + ' does not exist'));
+    }
+  });
+}
+
+exports.findByUsername = function(username, cb) {
+  process.nextTick(function() {
+    for (var i = 0, len = records.length; i < len; i++) {
+      var record = records[i];
+      if (record.username === username) {
+        return cb(null, record);
+      }
+    }
+    return cb(null, null);
+  });
+}
