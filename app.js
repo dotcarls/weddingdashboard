39e7992230acfed99112d226fdb326662f6d7d7c
Tim Carlson <tim.carlson@velocloud.net>
2016-04-13 22:17:51 -0400

    added mongostore

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const http         = require('http'),
       express      = require('express'),
       passport     = require('passport'),
       Strategy     = require('passport-local').Strategy,
-      auth         = require('./auth');
+      auth         = require('./auth'),
+      session      = require('express-session'),
+      MongoStore   = require('connect-mongo')(session);
 
 passport.use(new Strategy(
   function(username, password, cb) {
@@ -31,13 +33,23 @@ passport.deserializeUser(function(id, cb) {
   });
 });
 
+var connection_string = '127.0.0.1:27017/weddingdashboard';
+// if OPENSHIFT env variables are present, use the available connection info:
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
+  connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+  process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+  process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+  process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+  process.env.OPENSHIFT_APP_NAME;
+}
+
 const app = express();
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(require('morgan')('combined'));
 app.use(require('cookie-parser')());
 app.use(require('body-parser').urlencoded({ extended: true }));
-app.use(require('express-session')({ secret: 'timnmaddy', resave: false, saveUninitialized: false }));
+app.use(session({resave: false, saveUninitialized: false, secret: 'timnmaddy', store: new MongoStore({url: 'mongodb://' + connection_string})}));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static('public'));
@@ -46,7 +58,7 @@ app.get('/health', function(req, res) {
   res.status(200).send(200);
 });
 
-app.get('/', function(req, res) {
+app.get('/', require('connect-ensure-login').ensureLoggedIn(), function(req, res) {
   res.render('home', {user: req.user});
 });
 
